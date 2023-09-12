40bf0ce43b5985d0709559d507dfd764c2ae85be
Hyeonki Hong <hhk7734@gmail.com>
2023-09-13 00:19:22 +0900

    feat: change to svelte

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const withNextIntl = require("next-intl/plugin")("./src/i18n/i18n.tsx");
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = withNextIntl(nextConfig);
--- src/i18n/i18n.tsx
+++
@@ -1,8 +0,0 @@
-import { getRequestConfig } from "next-intl/server";
-
-export default getRequestConfig(async ({ locale }) => ({
-  defaultTranslationValues: {
-    br: () => <br />,
-  },
-  messages: (await import(`./${locale}.json`)).default,
-}));
